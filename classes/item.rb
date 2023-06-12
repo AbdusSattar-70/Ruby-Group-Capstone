400bf8b8ddcb177b8c5dedd98db1a1adb0ab72f2
david30-maker <davidokpah396@gmail.com>
2023-06-12 15:18:50 +0100

    added author and achive method

--- classes/item.rb
+++ classes/item.rb
@@ -11,4 +11,19 @@ class Item
     @genre = genre
     genre.add_item(self) unless genre.add_item.include?(self)
   end
+
+  def author=(author)
+    @author = author
+    author.add_item(self) unless author.add_item.include?(self)
+  end
+
+  def label=(label)
+    @label = label
+    label.add_item(self) unless label.add_item.include?(self)
+  end
+
+  def move_to_archive
+    @archived = can_be_archived?
+
+  end
 end
