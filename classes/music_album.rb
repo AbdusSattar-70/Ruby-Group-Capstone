cf1ca9d78e964867e936e98cc5be86085b2be1e6
krisbobo <kiyongbohbo@gmail.com>
2023-06-13 16:42:07 +0100

    Add MusicAlbum class
    The class can create and instance object with a name, published date, genre and on_spotify default value of false. The class can be archived if published date is greater than 10 years and it is on spotify

---
+++ classes/music_album.rb
@@ -0,0 +1,16 @@
+require_relative './item'
+
+class MusicAlbum < Item
+  attr_accessor :name, :genre, :on_spotify, :publish_date
+  
+  def initialize(@name, publish_date, @genre, on_spotify: false)
+    super(publish_date)
+    @name = name
+    @genre = genre
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    super() && on_spotify
+  end
+end
