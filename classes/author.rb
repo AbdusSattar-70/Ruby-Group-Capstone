362dbdb21d7e6fab85e31b472c86fd886fb20e53
david30-maker <davidokpah396@gmail.com>
2023-06-13 13:15:59 +0100

    fix linter error

--- classes/author.rb
+++ classes/author.rb
@@ -4,14 +4,15 @@ class Author < Item
   attr_reader :id, :first_name, :last_name, :books
 
   def initialize(first_name, last_name)
+    super()
     @id = Random.rand(1..1000)
     @first_name = first_name
     @last_name = last_name
-    @books = []
+    @items = []
   end
 
-  def add_book(book)
-    @books << book
-    book.author = self
+  def add_item(item)
+    @items << item
+    item.author = self
   end
 end
